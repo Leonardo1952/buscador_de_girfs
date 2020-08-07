25d425a4e47d711fc1ef279d8a8e9e15c9799833
Leonardo1952 <lo88980@gmail.com>
2020-08-07 14:17:33 -0300

    Compartilhamento do gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 import 'gif_page.dart';
 
@@ -123,6 +124,9 @@ class _HomePageState extends State<HomePage> {
                   MaterialPageRoute(
                       builder: (context) =>
                           GifPage(snapshot.data["data"][index])));
+            },
+            onLongPress: () {
+              Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
             },
           );
         else
