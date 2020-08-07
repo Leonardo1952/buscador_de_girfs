3a503e35fda0b07035999fcaad183e0ab6d87ba8
Leonardo1952 <lo88980@gmail.com>
2020-08-07 14:06:26 -0300

    Criando e Trocando de Tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buscador_de_girfs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:buscador_de_girfs/ui/home_page.dart';
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
+import 'gif_page.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -17,8 +19,6 @@ class _HomePageState extends State<HomePage> {
 
   Future<Map> _getGifs() async {
     http.Response response;
-
-
 
     if (_seach == null)
       response = await http.get(
@@ -34,9 +34,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
-    _getGifs().then((map) {
-      print(map);
-    });
+    _getGifs().then((map) {});
   }
 
   @override
@@ -80,8 +78,8 @@ class _HomePageState extends State<HomePage> {
                         height: 200.0,
                         alignment: Alignment.center,
                         child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                              Colors.white),
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
                           strokeWidth: 5.0,
                         ),
                       );
@@ -91,8 +89,7 @@ class _HomePageState extends State<HomePage> {
                       else
                         return _createGifTable(context, snapshot);
                   }
-                }
-            ),
+                }),
           )
         ],
       ),
@@ -111,10 +108,7 @@ class _HomePageState extends State<HomePage> {
     return GridView.builder(
       padding: EdgeInsets.all(10.0),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0
-      ),
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
       itemCount: _getCount(snapshot.data["data"]),
       itemBuilder: (context, index) {
         if (_seach == null || index < snapshot.data["data"].length)
@@ -122,26 +116,37 @@ class _HomePageState extends State<HomePage> {
             child: Image.network(
                 snapshot.data["data"][index]["images"]["fixed_height"]["url"],
                 height: 300.0,
-                fit: BoxFit.cover
-            ),
+                fit: BoxFit.cover),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          GifPage(snapshot.data["data"][index])));
+            },
           );
         else
           return Container(
             child: GestureDetector(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.add, color: Colors.white, size: 70.0,),
-                  Text("Carregando mais...",
-                    style: TextStyle(color: Colors.white, fontSize: 22.0),)
-                ],
-              ),
-              onTap: (){
-                setState(() {
-                  _offset +=19;
-                });
-              }
-            ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "Carregando mais...",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
+                  ],
+                ),
+                onTap: () {
+                  setState(() {
+                    _offset += 19;
+                  });
+                }),
           );
       },
     );
